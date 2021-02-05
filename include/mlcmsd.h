6cad404240b899f44d00b8faa3dfd138b2457198
mani8785 <h.msbu88@gmail.com>
2021-02-05 10:34:35 +0100

    Removed some redundant methods in mlcmsd library. The methods were related to read and write from h5 file which are now in a new library.

--- include/mlcmsd.h
+++ include/mlcmsd.h
@@ -105,38 +105,6 @@ public:
     void encoder_one_level(const bmat &qxB_bin, const LEVEL_INFO &info_level, bmat &plain_texts, ivec &plain_texts_decim, bvec &enc_data_hard);
     void encoder_two_levels(const bmat &qxB_bin, const LEVEL_INFO &info_level1, const LEVEL_INFO &info_level2, bmat &plain_texts_two_levels, ivec &plain_texts_decim, bvec &enc_data_hard_1, bvec &enc_data_hard_2);
     void encoder_three_levels(const bmat *qxB_bin, const LEVEL_INFO *info_level1, const LEVEL_INFO *info_level2, const LEVEL_INFO *info_level3, bmat *plain_texts_LSBs, ivec &plain_texts_decim, bvec *enc_data_hard_1, bvec *enc_data_hard_2, bvec *enc_data_hard_3);
-
-    /*
-        Read and write from/to h5 files for data
-    */
-    void load_test_data(string data_name, int *len_data, vec *XA, vec *XB);
-
-    /*
-        read and write from/to files frame by frame
-    */
-    void get_dataset_info(string Fname, string Dname, H5T_class_t &Ctype, int &Rank, hsize_t Dims[], size_t &type_size, bool Disp_flag);
-    void get_dataset_info(string Fname, string Dname, int &Rank, hsize_t Dims[], H5std_string &my_dtype, bool Disp_flag);
-    void reshape_h5(string FnameInp, string Dnames[], int numdatasets, string FnameOut);
-    void combine_datasets(string FnameInp, string Dnames[], int numdatasets, string FnameOut);
-
-    void read_col_i_from_h5(string Fname, string Dname, H5T_class_t &Ctype, hsize_t Count, int col_i, ivec &data_read);
-    void read_col_i_from_h5(string Fname, string Dname, H5T_class_t &Ctype, hsize_t Count, int col_i, vec &data_read);
-    void read_subset_of_1D_dataset_h5(string Fname, string Dname, H5T_class_t &Ctype, hsize_t Count, int col_i, ivec &data_read);
-    void read_subset_of_1D_dataset_h5(string Fname, string Dname, H5T_class_t &Ctype, hsize_t Count, int col_i, vec &data_read);
-
-    void add_dataset(H5File &hdf5file, string DATASET_NAME, PredType pre_def_type, hsize_t FSPACE_RANK, hsize_t Dims[], DataSpace &fspace, DataSet &dataset);
-
-    void write_to_1D_dataset_ith_frame(DataSet &dataset, DataSpace &fspace, PredType pre_def_type, hsize_t Count, size_t ith_frame, vec &ith_frame_Tx);
-    void write_to_1D_dataset_ith_frame(DataSet &dataset, DataSpace &fspace, PredType pre_def_type, hsize_t Count, size_t ith_frame, ivec &ith_frame_Tx);
-    void write_to_1D_dataset_ith_frame(DataSet &dataset, DataSpace &fspace, size_t type_size, hsize_t Count, size_t ith_frame, ivec &ith_frame_Tx);
-
-
-    void write_to_2D_dataset_ith_frame(DataSet &dataset, DataSpace &fspace, PredType pre_def_type, hsize_t Count, size_t ith_frame, bvec &ith_frame_Tx);
-
-
-    void write_to_2D_dataset_ith_index_mapping(DataSet &dataset, DataSpace &fspace, PredType pre_def_type, hsize_t Count, hsize_t dim, hsize_t zth_frame, bmat &buffer_mat);
-    void write_to_2D_dataset_ith_index_mapping(DataSet &dataset, DataSpace &fspace, string dtype, hsize_t Count, hsize_t dim, hsize_t zth_frame, bmat &buffer_mat);
-
 };
 
 #endif /* class MLCMSD */
